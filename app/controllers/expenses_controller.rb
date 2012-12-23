56f9d09d6e583e486c959f167992b43182ece8d9
rilian <dmitriyis@gmail.com>
2012-12-23 17:58:32 +0200

    show month, show expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,4 +2,31 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.all
   end
+
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    if @expense.save
+      redirect_to expenses_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @expense.update_attributes(params[:expense])
+      redirect_to expense_path(@expense)
+    else
+      render :edit
+    end
+  end
 end
--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -2,4 +2,30 @@ class MonthsController < ApplicationController
   def index
     @months = Month.includes(:month_expenses)
   end
+
+  def show
+    @month = Month.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    if @month.save
+      redirect_to months_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @month.update_attributes(params[:month])
+      redirect_to month_path(@month)
+    else
+      render :edit
+    end
+  end
 end
