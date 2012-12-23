9d0ba993fe5d4bf769b85e5b71187f410add7b28
rilian <dmitriyis@gmail.com>
2012-12-23 17:16:09 +0200

    seed refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,8 @@ models = %w(Expense Month MonthExpense)
 
 models.each do |model|
   puts "Seeding #{model.underscore.pluralize}"
-  query = File.read(File.open(File.join(Rails.root, "db", "seeds", "#{model.underscore.pluralize}.sql"), 'r'))
-  puts query
-  ActiveRecord::Base.connection.execute query
+  seed_file = File.join(Rails.root, "db", "seeds", "#{model.underscore.pluralize}.sql")
+  ActiveRecord::Base.connection.execute File.read(File.open(seed_file), 'r') if File.exists? seed_file
 end
 
 models.each do |model|
